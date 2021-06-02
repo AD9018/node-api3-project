74f17cdfb3b8e7ade9399c8d9b33a2fea19bfc25
AD9018 <adamdadi23@gmail.com>
2021-06-02 16:51:19 -0400

    WIP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,56 @@
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} request to ${url} endpoint!`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await Users.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body;
+  try {
+    if (!name || !name.trim()) {
+      res.status(400).json({ message: "missing required name field" });
+    } else {
+      req.name = name.trim();
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body;
+  try {
+    if (!text || !text.trim()) {
+      res.status(400).json({ message: "missing required text feild" });
+    } else {
+      req.text = text.trim();
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validatePost, validateUser };
--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-
+const express = require("express");
+const usersRouter = require("./users/users-router");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+server.use("/api/users", usersRouter);
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    note: "Error located in the User Router",
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 module.exports = server;
